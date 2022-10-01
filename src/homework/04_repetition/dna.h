a754b94d779f454201d1d831dfb8ab611a35f27f
20221337COSC001816 <112662502+20221337COSC001816@users.noreply.github.com>
2022-10-01 11:50:26 -0500

    Homework4 testCommit1

--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -3,3 +3,14 @@
 /*
 Write prototype for function(s)
 */
+
+
+//write function prototype for an int value-return function named factorial that accepts an int parameter named num. 
+int factorial(int num);//Receive the 1 int parameter named num, return an integer.
+
+
+
+
+
+//write function prototype for an int value-return function named gcd that accepts two int parameters.
+int gcf(int param1, int param2);//Receive the 2 int parameter, return an integer.
